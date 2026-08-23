05bd02925046ea5681c1fb094ebe174fe52d535e
rightware-corporations <rightware123@gmail.com>
2026-08-23 19:25:38 +0200

    Disable default Spring user auto-configuration

--- backend/src/main/java/com/rightware/verox/VeroxApplication.java
+++ backend/src/main/java/com/rightware/verox/VeroxApplication.java
@@ -2,8 +2,9 @@ package com.rightware.verox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class VeroxApplication {
 
     public static void main(String[] args) {
